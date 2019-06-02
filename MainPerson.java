3bbb4dd8d40c15f41801477270708cacaa5d1c10
vramam <vramanji553@gmail.com>
2019-06-02 11:50:59 +0530

    added new person to Main Person

--- MainPerson.java
+++ MainPerson.java
@@ -1,17 +1,28 @@
 package com.git.demo;
 
+import com.git.demo.Person;
+
 public class MainPerson {
 
 	public static void main(String[] args) {
-		
-		Person ram=new Person();
+
+		// Ramanji Person details
+		Person ram = new Person();
 		ram.setPid(2047);
-		int ramId=ram.getPid();
-		System.out.println("Person Ram id "+ramId);
+		int ramId = ram.getPid();
+		System.out.println("Person Ram id " + ramId);
 		ram.setpName("Vazza Ramanjaneyulu");
-		String ramName=ram.getpName();
-		System.out.println("Person Ram Name "+ramName);
-		
+		String ramName = ram.getpName();
+		System.out.println("Person Ram Name " + ramName);
+
+		// Hari person Details
+		Person hari = new Person();
+		hari.setPid(2048);
+		int hariId = hari.getPid();
+		System.out.println("Person hari id " + hariId);
+		hari.setpName("Challa HariKrishna");
+		String hariName = hari.getpName();
+		System.out.println("Person hari Name " + hariName);
 
 	}
 
